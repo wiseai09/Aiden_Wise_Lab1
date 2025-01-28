1aef018b252884a10519c441a447672e1eb95a44
wiseai09 <wiseai@cwu.edu>
2025-01-28 15:35:05 -0800

    Upload create_files_with_subdirs.sh

--- create_files_with_subdirs.sh
+++ create_files_with_subdirs.sh
@@ -6,15 +6,17 @@ languages=("Python" "Java" "JavaScript" "C" "C++" "C#" "Assembly" "Fortran" "HTM
 
 for i in {101..110}; do
         mkdir -p $parent_dir/file$i
-done
 
-echo "${languages[1]}" > $parent_dir/file101/tuser510.txt
-echo "${languages[2]}" > $parent_dir/file102/tuser510.txt
-echo "${languages[3]}" > $parent_dir/file103/tuser510.txt
-echo "${languages[4]}" > $parent_dir/file104/tuser510.txt
-echo "${languages[5]}" > $parent_dir/file105/tuser510.txt
-echo "${languages[6]}" > $parent_dir/file106/tuser510.txt
-echo "${languages[7]}" > $parent_dir/file107/tuser510.txt
-echo "${languages[8]}" > $parent_dir/file108/tuser510.txt
-echo "${languages[9]}" > $parent_dir/file109/tuser510.txt
-echo "${languages[10]}" > $parent_dir/file110/tuser510.txt
+        for j in {501..510}; do
+                echo "${languages[1]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[2]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[3]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[4]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[5]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[6]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[7]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[8]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[9]}" > $parent_dir/file$i/tuser$j.txt
+                echo "${languages[10]}" > $parent_dir/file$i/tuser$j.txt
+        done
+done
